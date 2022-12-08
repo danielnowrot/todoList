4f909bec3118f65a1ef45099c942cdaacf897d76
Daniel Nowrot <daniel.nowrot@gmail.com>
2022-12-08 11:11:58 +0100

    remove tasks

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
       },
       {
          content: "zadanie 2",
-         done: false,
+         done: true,
       },
    ];
 
@@ -15,19 +15,55 @@
 
       for (const task of tasks) {
          htmlString += `
-         <li>
+         <li 
+            ${task.done ? "style =\"text-decoration: line-through\"" : ""}
+         >
+         <button class="js-remove">Usuń</button>
          ${task.content}
          </li>
          `;
       };
 
       document.querySelector(".js-tasksList").innerHTML = htmlString;
+      const removeButtons = document.querySelectorAll(".js-remove");
+
+      removeButtons.forEach((removeButton,index)=>{
+         removeButton.addEventListener("click",() =>{
+            removeTask(index);
+         })
+      })
    };
 
-   
+   const addNewTask = (newTaskContent) => {
+      tasks.push({
+         content: newTaskContent,
+      })
+
+      render();
+   }
+
+   const removeTask = (index) =>{
+      tasks.splice(index,1)
+            render();
+   }
+
+   const onFormSubmit = (event) => {
+      event.preventDefault();
+
+      const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+      if (newTaskContent === "") {
+         return;
+      }
+      addNewTask(newTaskContent);
+   }
 
    const init = () => {
       render();
-   }
+
+      const form = document.querySelector(".js-form");
+
+      form.addEventListener("submit", onFormSubmit);
+   };
    init();
 }
